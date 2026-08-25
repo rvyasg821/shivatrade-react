ef307cefdae376bc00a31be5ce9fa29436d933da
JugalMahida <jugal.strivedge@gmail.com>
2026-08-25 14:47:01 +0530

    Add Revert action in the POV

--- src/views/po-vendors/view/PoVendorTimelinePanel.js
+++ src/views/po-vendors/view/PoVendorTimelinePanel.js
@@ -49,6 +49,7 @@ const PoVendorTimelinePanel = ({ height }) => {
       povActionFlag === "POV_DISPATCHED" ||
       povActionFlag === "POV_CLOSED" ||
       povActionFlag === "POV_CANCELLED" ||
+      povActionFlag === "POV_REVERTED" ||
       povActionFlag === "POV_UPDT" ||
       povActionFlag === "POV_PAY_SCS" ||
       povActionFlag === "POV_PAY_VOID_SCS"
